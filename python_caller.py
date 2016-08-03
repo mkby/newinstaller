76f48a63e400cc608283de353dbd5815e7e5a123
Eason <hfutmkby@gmail.com>
2016-08-03 17:06:54 +0800

    update python_caller

--- python_caller.py
+++ python_caller.py
@@ -1,77 +1,98 @@
 #!/usr/bin/env python
 
-from common import ParseJson, ok, err, info, time_elapse
+from common import *
 from threading import Thread
 import subprocess
 import getpass
 import os, socket
 
 class Remote:
-    ''' run commands or scripts remotely using ssh '''
-    def __init__(self, host):
+    """ run commands or scripts remotely using ssh """
+    def __init__(self, host, pwd):
         self.host = host
-        self.pwd = ''
+        self.pwd = pwd
+        self.chkssh_cmd = 'exit 0'
+        self.tmp_folder = '.install'
+        self.sshpass = self.__sshpass_available()
 
-
-    def __put_file(self, src_file, dest_file):
+    def __commands(self, method):
+        """ method should be 'ssh' or 'scp' """
+        cmd = []
+        if self.sshpass and self.pwd: cmd = ['sshpass','-p', self.pwd]
+        cmd += [method]
+        if not self.pwd: cmd += ['-oPasswordAuthentication=no']
+        return cmd
+        
+    def __put_file(self, src_file, dest_file=''):
         if not os.path.exists(src_file):
             err('Script %s doesn\'t exist' % src_file)
-        cmd = []
-        if sshpass_available() and self.pwd: cmd = ['sshpass','-p', self.pwd]
-        cmd += ['scp']
+        if not dest_file: dest_file = src_file
+
+        cmd = self.__commands('scp')
         cmd += [src_file]
-        cmd += ['%s:~/%s' % (self.host, dest_file)]
+        cmd += ['%s:~/%s/%s' % (self.host, self.tmp_folder, dest_file)]
         try:
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             p.communicate()
         except Exception as e:
             err('Failed to copy file to remote host: %s' %  e)
 
-    def run_script(self, script_file, pwd='', script_options='', verbose=False):
-        self.pwd = pwd
+    def run_script(self, script_file, script_options='', verbose=False):
+
+        # check host connectivity first
+        self.__run_sshcmd(self.chkssh_cmd)
+
+        # create tmp folder
+        self.__run_sshcmd('mkdir -p ~/%s' % self.tmp_folder)
 
         # copy file to remote
-        target_file = '.' + script_file.split('/')[-1]
+        target_file = script_file.split('/')[-1]
         self.__put_file(script_file, target_file)
+        self.__put_file('common.py')
 
         # set permission
-        self.run_cmd('chmod a+rx ' + target_file)
-        cmd = '~/' + target_file + ' ' + script_options
-        output = self.run_cmd(cmd)
+        self.__run_sshcmd('chmod a+rx %s/%s' % (self.tmp_folder, target_file))
+
+        # run script
+        cmd = '~/%s/%s' % (self.tmp_folder, target_file)
+        if script_options: cmd += ' ' + script_options 
+        output = self.__run_sshcmd(cmd)
         if verbose: print output
 
         ok('Host [%s]: script \'%s\' running successfully!' % (self.host, script_file))
-        #remove tmp file
-        self.run_cmd('rm -rf ' + target_file)
+
+        #remove tmp folder
+        self.__run_sshcmd('rm -rf %s' % self.tmp_folder)
             
         
-    def run_cmd(self, user_cmd):
+    def __run_sshcmd(self, user_cmd):
         user_cmds = user_cmd.split()
-        cmd = []
-        if sshpass_available() and self.pwd: cmd = ['sshpass','-p', self.pwd]
-        cmd += ['ssh']
-        cmd += ['-q', '-oStrictHostKeyChecking=no']
+        cmd = self.__commands('ssh')
         cmd += [self.host]
-        # issue on sh -c command to run 'hostname -s' 
-        #cmd += ['sh', '-c']
         cmd += user_cmds
 
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout = p.communicate()[0]
+        stdout, stderr = p.communicate()
         if p.returncode != 0:
-            err('Host [%s]: Failed to run command: %s' % (self.host, user_cmd))
+            if user_cmd == self.chkssh_cmd:
+                err('Host [%s]: Failed to connect using SSH! Check password or connectivity.' % (self.host))
+            else:
+                msg = 'Host [%s]: Failed to run \'%s\'' % (self.host, user_cmd.split('/')[-1])
+                if stderr: msg += ': ' + stderr
+                get_logger().error(msg)
+                err(msg)
         return stdout
 
 
-def sshpass_available():
-    sshpass_available = True
-    try:
-        p = subprocess.Popen(['sshpass'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        p.communicate()
-    except OSError:
-        sshpass_available = False
+    def __sshpass_available(self):
+        sshpass_available = True
+        try:
+            p = subprocess.Popen(['sshpass'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p.communicate()
+        except OSError:
+            sshpass_available = False
 
-    return sshpass_available
+        return sshpass_available
 
 class Status:
     def __init__(self, stat_file, name):
@@ -94,18 +115,20 @@ class Status:
 def run(cfgs, options):
     stat_file = 'install.status'
     cfg_file = 'script_config.json'
-    #if sshpass_available() and not options['pwd']:
-    if sshpass_available():
+
+    enable_pwd = True
+    #enable_pwd = False
+    if enable_pwd:
         pwd = getpass.getpass('Input SSH Password: ')
+    else:
+        pwd = ''
 
     conf = ParseJson(cfg_file).jload()
     script_cfgs = conf['conf']
 
-    hosts = ['cent-1']
-    remote_instances = [Remote(host) for host in hosts]
+    hosts = ['eason-1', 'cent-2']
+    remote_instances = [Remote(host, pwd) for host in hosts]
 
-    # TODO: run host checking first
-    
     for cfg in script_cfgs:
         script = cfg['script']
         node = cfg['node']
@@ -115,11 +138,11 @@ def run(cfgs, options):
             continue
 
         if node == 'local':
-            Remote(socket.gethostname()).run_script(script, pwd)
+            Remote(socket.gethostname(), pwd).run_script(script)
         if node == 'first':
-            Remote(hosts[0]).run_script(script, pwd)
+            remote_instances[0].run_script(script)
         elif node == 'all':
-            threads = [Thread(target=r.run_script, args=(script, pwd)) for r in remote_instances]
+            threads = [Thread(target=r.run_script, args=(script, )) for r in remote_instances]
             # TODO: thread return code
             for t in threads: t.start()
             for t in threads: t.join()
