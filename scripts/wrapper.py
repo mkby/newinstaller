33009b5ea46e2edd383822578d0d86582b3333ca
Eason <hfutmkby@gmail.com>
2016-12-12 22:45:13 +0800

    remove

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -156,10 +156,6 @@ def run(dbcfgs, options, mode='install', pwd=''):
     else:
         skipped_scripts += ['apache_mods', 'apache_restart']
 
-    # if no license file, ignore license check
-    if not dbcfgs.has_key('license_file'):
-        skipped_scripts += ['license_check']
-
     # set ssh config file to avoid known hosts verify on current installer node
     SSH_CFG_FILE = os.environ['HOME'] + '/.ssh/config'
     ssh_cfg = 'StrictHostKeyChecking=no\nNoHostAuthenticationForLocalhost=yes\n'
